2d285923472ed455fcf740bd2179d85bd0ba339f
Albraa A Ahmed <AlbraaEngineer@gmail.com>
2022-07-13 16:48:33 -0700

    extract properties from the store while keeping its reactivity,

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,26 @@
 <script setup>
+import { storeToRefs } from "pinia";
 import { useCounterStore } from "./stores/counter";
 import { useUserStore } from "./stores/user";
 
 const counterStore = useCounterStore();
 const userStore = useUserStore();
-counterStore.$subscribe((mutations, state)=>{
+counterStore.$subscribe((mutations, state) => {
   console.log(mutations);
   console.log(state);
 });
+
+const { name, age } = storeToRefs(userStore);
 </script>
 <template>
   <div>
     <h1>Counter : {{ counterStore.counter }}</h1>
 
     <h1>User : {{ userStore.name }}</h1>
+    <h1>MyUser : {{ name }}</h1>
+    <h1>MyAge : {{ age }}</h1>
+    <button @click="age++">Age++</button>
+
     <button @click="counterStore.counter++">+</button>
     <button @click="counterStore.$reset">Reset</button>
     <button
--- src/stores/user.js
+++ src/stores/user.js
@@ -2,6 +2,6 @@ import { defineStore } from "pinia";
 
 export const useUserStore = defineStore("user", {
   state: () => {
-    return { name: "Albraa A Ahmed", };
+    return { name: "Albraa A Ahmed", age:25};
   },
 });

b446efdc85127edd9a29616f3be6bb0b068a3a6c
Albraa A Ahmed <AlbraaEngineer@gmail.com>
2022-07-14 03:01:45 -0700

    add getters and actions to counterstore

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -2,6 +2,21 @@ import { defineStore } from "pinia";
 
 export const useCounterStore = defineStore("counter", {
   state: () => {
-    return { counter: 0, };
+    return { counter: 0 };
+  },
+  getters: {
+    getCounter() {
+      return this.counter;
+    },
+    getDoubleCounter: (state) => state.counter * 2,
+    getTrippleCounter() {
+      return this.getCounter * 3;
+    },
+    multiplyBy: (state) => (number) => state.counter * number,
+  },
+  actions: {
+    increment() {
+      this.counter++
+    },
   },
 });
